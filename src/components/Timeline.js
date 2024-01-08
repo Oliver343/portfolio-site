bede2e71067b2078bf16297880c11af488c4adb2
Oliver343 <oliver_343@hotmail.com>
2024-01-08 12:05:39 +0100

    MVP

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,15 @@
 import './InfoBox.css';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBusinessTime } from '@fortawesome/free-solid-svg-icons'
+import { faBusinessTime, faSquareCaretDown, faSquareCaretUp } from '@fortawesome/free-solid-svg-icons'
 
 function Timeline() {
+  const [expandOne, setExpandOne] = useState(true)
+  const [expandTwo, setExpandTwo] = useState(true)
+  const [expandThree, setExpandThree] = useState(true)
+  const [expandFour, setExpandFour] = useState(true)
+  const [expandFive, setExpandFive] = useState(true)
+  const [expandSix, setExpandSix] = useState(true)
 
   return (
     <div className="InfoBox">
@@ -11,40 +18,52 @@ function Timeline() {
       </div>
       <div className="infoText" >
         <br />
-        4Geeks - Full-Stack Software Developer Course - (2023) 
-        <ul>
+        My work and education history is listed below. Click on an option to see the related skills.
+        <br />
+        <br />
+        <button className="projectButton" onClick={() => {setExpandOne(!expandOne) }}>4Geeks - Full-Stack Software Developer Course - (2023) &nbsp; <FontAwesomeIcon icon={expandOne ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandOne}>
           <li>React</li>
           <li>Python</li>
           <li>JavaScript</li>
           <li>HTML/CSS</li>
         </ul>
-        Next Flow - Angular Developer - (2022 - 2023) 
-        <ul>
+        <br hidden={!expandOne}/>
+        <button className="projectButton" onClick={() => {setExpandTwo(!expandTwo) }}>Next Flow - Angular Developer - (2022 - 2023)  &nbsp; <FontAwesomeIcon icon={expandTwo ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandTwo}>
           <li>Angular</li>
           <li>TypeScript</li>
           <li>JavaScript</li>
           <li>HTML/CSS</li>
         </ul>
-        Veiovia Ltd - Junior Software Developer - (2022) 
-        <ul>
+        <br hidden={!expandTwo}/>
+        <button className="projectButton" onClick={() => {setExpandThree(!expandThree) }}>Veiovia Ltd - Junior Software Developer - (2022)   &nbsp; <FontAwesomeIcon icon={expandThree ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandThree}>
           <li>React</li>
           <li>Python</li>
           <li>JavaScript</li>
           <li>Web3</li>
         </ul>
-        Ministry of Defense - Data Analyst - (2019 - 2021) 
-        <ul>
+        <br hidden={!expandThree}/>
+        <button className="projectButton" onClick={() => {setExpandFour(!expandFour) }}>Ministry of Defense - Data Analyst - (2019 - 2021)    &nbsp; <FontAwesomeIcon icon={expandFour ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandFour}>
           <li>SQL</li>
           <li>Database</li>
         </ul>
-        Ebuyer - Product Supervisor / Technical Support Supervisor - (2013 - 2019) 
-        <ul>
+        <br hidden={!expandFour}/>
+        <button className="projectButton" onClick={() => {setExpandFive(!expandFive) }}>Ebuyer - Product Supervisor / Technical Support Supervisor - (2013 - 2019)    &nbsp; <FontAwesomeIcon icon={expandFive ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandFive}>
           <li>JavaScript</li>
           <li>HTML/CSS</li>
         </ul>
-        Computer Science Degree - University of Hull, England- (2011)
-        <br />
-        <br />
+        <br hidden={!expandFive}/>
+        <button className="projectButton" onClick={() => {setExpandSix(!expandSix) }}>Computer Science Degree - University of Hull, England- (2011)    &nbsp; <FontAwesomeIcon icon={expandSix ? faSquareCaretDown : faSquareCaretUp} />&nbsp;</button>
+        <ul hidden={expandSix}>
+          <li>JavaScript</li>
+          <li>HTML/CSS</li>
+          <li>SQL</li>
+        </ul>
+        <br hidden={!expandSix}/>
         <br />
       </div>
 
