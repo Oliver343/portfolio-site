906dbb537862842782bd925b665c37d2e10bcccd
Oliver343 <oliver_343@hotmail.com>
2024-01-08 13:58:06 +0100

    Packages installed

--- src/pages/Base.js
+++ src/pages/Base.js
@@ -10,7 +10,7 @@ import projectStack from '../img/projectFrame.png'
 import timelineStack from '../img/timelineFrame.png'
 import contactStack from '../img/contactFrame.png'
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight, faCaretLeft, faEnvelope, faFaceSmile, faGear, faFolder, faBusinessTime, faCircleDown } from '@fortawesome/free-solid-svg-icons'
 import documentCV from '../documents/OliverSandersonCV.pdf'
 
